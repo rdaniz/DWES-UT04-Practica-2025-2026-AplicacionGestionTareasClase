24a1c93f63115ae4adaf60459b88dbdf26aebdad
rdaniz <dani_pmz@hotmail.com>
2026-01-10 18:44:16 +0100

    Registrar modelos en admin.py, añadir URLs de la aplicación TareasDWES, crear vista ejemplo bienvenida.

--- App_TareasClase/TareasDWES/admin.py
+++ App_TareasClase/TareasDWES/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Usuario, Tarea, TareaGrupal
 
 # Register your models here.
+from django.contrib import admin
+
+admin.site.register(Usuario)
+admin.site.register(Tarea)
+admin.site.register(TareaGrupal)
