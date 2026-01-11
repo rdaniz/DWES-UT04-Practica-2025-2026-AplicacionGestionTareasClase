53ff0ae4631a3105b8bc85fbb76ef20526f87950
rdaniz <dani_pmz@hotmail.com>
2026-01-11 13:29:28 +0100

    Crear vista ver datos, template para la vista y registrar url

--- App_TareasClase/TareasDWES/urls.py
+++ App_TareasClase/TareasDWES/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from .views import ListadoUsuariosView, index
+from .views import DetalleUsuarioView, ListadoUsuariosView, index
 # listado_usuarios
 
 urlpatterns = [
       path('', index, name='index'),  # Página principal de TareasDWES
       # path('usuarios/', listado_usuarios, name='listado_usuarios'),  # Listado de usuarios
       path('usuarios/', ListadoUsuariosView.as_view(), name='listado_usuarios'),  # Listado de usuarios ListView
+      #path('mis-datos/<int:pk>/', mis_datos, name='mis_datos'),  # Mis datos (los obtengo por pk id)
+      path('mis-datos/<int:pk>/', DetalleUsuarioView.as_view(), name='mis_datos'),  # Mis datos (los obtengo por pk id) DetailView
 ]
--- App_TareasClase/TareasDWES/views.py
+++ App_TareasClase/TareasDWES/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import Usuario
 
@@ -26,3 +26,32 @@ class ListadoUsuariosView(ListView):
     model = Usuario
     template_name = 'listado_usuarios.html'  # Template
     context_object_name = 'usuarios'        # Nombre de la variable en el template
+
+    def get_queryset(self):
+        return Usuario.objects.all().order_by('username')  # Ordenar por nombre de usuario
+
+
+
+# VISTA MIS DATOS
+
+# def mis_datos(request, pk):
+#    try:
+#        usuario = Usuario.objects.get(pk=pk)
+#    except Usuario.DoesNotExist:
+#        return render(request, 'mis_datos.html', {
+#            'error': "Usuario incorrecto"
+#        })
+
+#   return render(request, 'mis_datos.html', {
+#        'usuario': usuario
+#    })
+
+class DetalleUsuarioView(DetailView):
+    model = Usuario
+    template_name = 'mis_datos.html'
+    context_object_name = 'datos_usuario'
+
+    def get_object(self, queryset=None):
+        # Devuelve el usuario si existe, si no None
+        return Usuario.objects.filter(pk=self.kwargs.get('pk')).first()
+
