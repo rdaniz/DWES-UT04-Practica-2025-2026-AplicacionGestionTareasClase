59f3e78cfdbb67f2480b760892580467aebf6ea4
rdaniz <dani_pmz@hotmail.com>
2026-01-11 11:34:56 +0100

    Mejorar templates index.html y listado_usuarios.html

--- App_TareasClase/TareasDWES/urls.py
+++ App_TareasClase/TareasDWES/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import index, listado_usuarios
+from .views import ListadoUsuariosView, index
+# listado_usuarios
 
 urlpatterns = [
       path('', index, name='index'),  # Página principal de TareasDWES
-      path('usuarios/', listado_usuarios, name='listado_usuarios'),  # Listado de usuarios
+      # path('usuarios/', listado_usuarios, name='listado_usuarios'),  # Listado de usuarios
+      path('usuarios/', ListadoUsuariosView.as_view(), name='listado_usuarios'),  # Listado de usuarios ListView
 ]
--- App_TareasClase/TareasDWES/views.py
+++ App_TareasClase/TareasDWES/views.py
@@ -1,14 +1,28 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import ListView
 
 from .models import Usuario
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Bienvenido a la aplicación de gestión de tareas.")
 
-# Vista listado de usuarios
-def listado_usuarios(request):
-    usuarios = Usuario.objects.all()  # Trae todos los usuarios
-    return render(request, 'listado_usuarios.html', {'usuarios': usuarios})
-   
+# VISTA INDEX
+
+# def index(request):
+#     return HttpResponse("Bienvenido a la aplicación de gestión de tareas.")
+
+def index(request):
+    return render(request, 'index.html')
+
+
+
+# VISTA LISTADO DE USUARIOS
+
+# def listado_usuarios(request):
+#     usuarios = Usuario.objects.all()  # Trae todos los usuarios
+#     return render(request, 'listado_usuarios.html', {'usuarios': usuarios})
+
+class ListadoUsuariosView(ListView):
+    model = Usuario
+    template_name = 'listado_usuarios.html'  # Template
+    context_object_name = 'usuarios'        # Nombre de la variable en el template
